5b3e7d4f791ee8f41effaf4399a6c719433e00f2
kumar2106 <ka09934147002@gmail.com>
2023-01-04 20:47:44 +0530

    Intial commit

---
+++ src/test/java/com/example/code_decode/CodeDecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_decode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
